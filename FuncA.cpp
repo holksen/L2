f2944964d3f238acc7f6bb822e06660be2610002
holksen <cheryxso@gmail.com>
2024-10-28 10:22:49 +0200

    Update FuncA.cpp

--- FuncA.cpp
+++ FuncA.cpp
@@ -1,6 +1,11 @@
 #include "FuncA.h"
 #include <cmath>
 
+//Ф-ія обчислює значення суми перших n елементів ряду для ln(1 + x)
+//Параметри:
+// x - значення, для якого обчислюється логарифм
+// n - кл-ть елементів ряду для обчислення
+
 double FuncA::calculate(double x, int n) {
   double sum = 0.0;
   for (int i = 1; i <= n; ++i) {
