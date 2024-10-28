1ea406eccd9533d9c68a03afb399de64ff948787
holksen <cheryxso@gmail.com>
2024-10-28 10:09:15 +0200

    UpdateFuncA.cpp

--- FuncA.cpp
+++ FuncA.cpp
@@ -1,5 +1,11 @@
 #include "FuncA.h"
+#include <cmath>
 
 double FuncA::calculate(double x, int n) {
-  return 0;
+  double sum = 0.0;
+  int terms = (n < 3) ? n : 3;
+  for (int i = 1; i <= terms; ++i) {
+    sum += pow(-1, i - 1) * pow(x, i) / i;
+  }
+  return sum;
 }
